8bb8cd149346b015caf046c46ea4c3dff17b7791
akeanti <162023213+akeanti@users.noreply.github.com>
2024-09-02 17:04:11 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ import config
 class TuxBot(commands.Bot):
 
     def __init__(self):
-        super().__init__(command_prefix="!", intents=discord.Intents.all())
+        super().__init__(command_prefix="/", intents=discord.Intents.all())
         self.status_cycle = [
             "playing with Linux", "destroying Windows",
             "use /help to get started..."
@@ -33,10 +33,39 @@ class TuxBot(commands.Bot):
             "cogs.linux",
             "cogs.general",
             "cogs.cards",
-            "cogs.ticket"  # Add ticket cog here
+            "cogs.ticket",  # Ticket cog
+            "cogs.utility",  # Utility cog
         ]
         for cog in cogs:
             await self.load_extension(cog)
+
+    async def on_guild_join(self, guild):
+        owner = guild.owner
+        if owner:
+            # Create a more visually appealing embed
+            embed = discord.Embed(
+                title="🎉 Thanks for Using Tux!",
+                description=
+                f"Thank you for adding Tux Bot to your server, **{guild.name}**! We're thrilled to be here.",
+                color=discord.Color.orange())
+            embed.set_thumbnail(
+                url=
+                "https://media.discordapp.net/attachments/1278052479308664884/1279518988224954488/shouko-nishimiya-arigato.gif?ex=66d4bc6b&is=66d36aeb&hm=749625411f78ec5b827638dbe553f4d841eadb22619852cec1c559dd6ea93786&=&width=622&height=330"
+            )
+            embed.set_footer(text="Tux Bot | Powered by Akeanti")
+
+            # Create a button
+            button = discord.ui.Button(
+                label="Visit Documentation",
+                url="https://tux-discord-bot.github.io/Documentation/")
+            view = discord.ui.View()
+            view.add_item(button)
+
+            # Send a DM to the guild owner
+            try:
+                await owner.send(embed=embed, view=view)
+            except discord.Forbidden:
+                print("Could not send DM to the guild owner.")
 
 
 bot = TuxBot()
